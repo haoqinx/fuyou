6a60309dbfc2b6a2bf293812a2f323fdde2101a3
haoqinx <363104497@qq.com>
2021-12-19 20:14:03 +0800

    update

--- fuyou/EventLoopThreadPool.cpp
+++ fuyou/EventLoopThreadPool.cpp
@@ -13,6 +13,23 @@ EventLoopThreadPool::EventLoopThreadPool(EventLoop*loop, int numThreads)
 }
 
 void EventLoopThreadPool::start(){
+    _mainLoop -> assertInLoopThread();
+    _started = true;
+    for(int i = 0; i < _numThreads; ++ i){
+        SP_LoopThread tmp(new EventLoopThread());
+        _threads.push_back(tmp);
+        _loops.push_back(tmp -> startloop());
+    }
+}
 
+EventLoop* EventLoopThreadPool::getNextLoop(){
+    _mainLoop -> assertInLoopThread();
+    assert(_started);
+    EventLoop* loop = _mainLoop;
+    if(! _loops.empty()){
+        loop = _loops[_next];
+        _next = (_next + 1) % _numThreads;
+    }
+    return loop;
 }
 } // namespace fuyou
