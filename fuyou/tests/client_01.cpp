bbb547c3b3c4752b1b7a2a403b7c57d40bfddfbe
haoqinx <363104497@qq.com>
2022-01-05 22:19:22 +0800

    update

--- fuyou/tests/client_01.cpp
+++ fuyou/tests/client_01.cpp
@@ -4,12 +4,14 @@
 #include <unistd.h>
 
 using namespace std;
+//线程执行函数
 void* func(void*args){
     cout << pthread_self() << endl;
     cout << ::syscall(SYS_gettid) << endl;
     return nullptr;
 }
 int main(){
+
     pthread_t p1;
     cout << "current process:" << ::syscall(SYS_gettid) << endl;
     pthread_create(&p1, nullptr, func, nullptr );
